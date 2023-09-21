c3c7c679e0db7eb5e05792edfa8368ae8fc1b0df
hybrid.simpler@gmail.com <hybrid.simpler@gmail.com>
2023-09-21 23:17:06 +0900

    Feat: BaseEntity를 사용하도록 애노테이션 추가

--- src/main/java/io/springbootweb/SpringBootWebApplication.java
+++ src/main/java/io/springbootweb/SpringBootWebApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @ConfigurationPropertiesScan
 public class SpringBootWebApplication {
 
